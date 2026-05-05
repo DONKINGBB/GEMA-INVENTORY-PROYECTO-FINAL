47b61976dd85f241395343beb5de07531ef29baa
DONKINGBB <elchancli10@gmail.com>
2026-05-05 16:30:55 -0600

    Configure production API URL for Render

--- GEMA_web/src/services/api.js
+++ GEMA_web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api', // Proxy handles redirection to http://localhost:8080
+    baseURL: 'https://gema-inventory-backend.onrender.com',
     headers: {
         'Content-Type': 'application/json',
     },
