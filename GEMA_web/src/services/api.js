2a0815e9e67559d97afe881349718defdfe6e279
DONKINGBB <elchancli10@gmail.com>
2026-05-05 16:51:44 -0600

    Fix API baseURL: add /api suffix

--- GEMA_web/src/services/api.js
+++ GEMA_web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://gema-inventory-backend.onrender.com',
+    baseURL: 'https://gema-inventory-backend.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
